ee915bfb3bae23997a95c9959abe81530c428e92
⚡Ben Krejci ⚡ <krejci@duolingo.com>
2022-04-20 19:18:21 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -10,28 +10,13 @@ sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 sudo apt-get install -y \
   curl \
   git \
-  gnupg2 \
-  jq \
   sudo \
   openssh-client \
   less \
-  iproute2 \
-  procps \
   wget \
   unzip \
-  apt-transport-https \
-  lsb-release 
-
-# Install Azure CLI
-# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/azure-cli.list
-# curl -sL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - 2>/dev/null
-# sudo apt-get update
-# sudo apt-get install -y azure-cli;
-
-# Install Jetbrains Mono font
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
-sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
-sudo fc-cache -f -v
+  lsb-release \
+  vim
 
 # Install & Configure Zsh
 if [ "$INSTALL_ZSH" = "true" ]
